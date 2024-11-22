7b93bf3ac1b5f1c7b8fcfb98b94d99d1e4447e8b
Artem Tyanutov <artem.lab@gmail.com>
2024-11-23 02:11:10 +0500

    co types

---
+++ cotypes.h
@@ -0,0 +1,15 @@
+#ifndef COTYPES_H
+#define COTYPES_H
+
+#include <stdint.h>
+
+namespace CO
+{
+
+typedef uint8_t  NodeId;
+typedef uint16_t Index;
+typedef uint8_t  SubIndex;
+
+}
+
+#endif // COTYPES_H
