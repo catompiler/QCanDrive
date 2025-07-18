67fdc0466a51a2162b95d21a03d098776242403e
catompiler <artem.lab@gmail.com>
2025-07-18 11:10:18 +0500

    added empty qt project

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
-#include "canopenwin.h"
-
+#include "mainwindow.h"
 #include <QApplication>
+
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    CanOpenWin w;
+    MainWindow w;
+    
     w.show();
     return a.exec();
 }
+
