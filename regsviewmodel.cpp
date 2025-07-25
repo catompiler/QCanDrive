8b6c41953ed0d50739af8c98f13d2cb287717c79
catompiler <artem.lab@gmail.com>
2025-07-25 16:38:12 +0500

    regs view stub proxy model

--- regsviewmodel.cpp
+++ regsviewmodel.cpp
@@ -4,9 +4,30 @@
 #include <QDebug>
 
 
+typedef struct _Col_Mapping {
+    const char* name;
+    int sourceCol;
+} col_mapping_t;
+
+
+static const col_mapping_t cols[] = {
+    {"Индекс", RegListModel::COL_INDEX},
+    {"Имя", RegListModel::COL_NAME},
+    {"Описание", RegListModel::COL_DESCR},
+    {"Тип", RegListModel::COL_TYPE},
+    {"Минимум", RegListModel::COL_MIN_VAL},
+    {"Максимум", RegListModel::COL_MAX_VAL},
+    {"По-умолчанию", RegListModel::COL_DEF_VAL},
+    {"Значение", -1},
+};
+
+
+static const auto col_count = sizeof(cols) / sizeof(cols[0]);
+
+
 
 RegsViewModel::RegsViewModel(QObject* parent)
-    :QSortFilterProxyModel(parent)
+    :QAbstractProxyModel(parent)
 {
 }
 
@@ -14,6 +35,61 @@ RegsViewModel::~RegsViewModel()
 {
 }
 
+
+QModelIndex RegsViewModel::index(int row, int column, const QModelIndex& parent) const
+{
+    QAbstractItemModel* model = sourceModel();
+    if(model == nullptr) return QModelIndex();
+
+    return model->index(row, column, parent);
+}
+
+QModelIndex RegsViewModel::parent(const QModelIndex& child) const
+{
+    QAbstractItemModel* model = sourceModel();
+    if(model == nullptr) return QModelIndex();
+
+    return model->parent(child);
+}
+
+int RegsViewModel::rowCount(const QModelIndex& parent) const
+{
+    QAbstractItemModel* model = sourceModel();
+    if(model == nullptr) return 0;
+
+    return model->rowCount(parent);
+}
+
+int RegsViewModel::columnCount(const QModelIndex& parent) const
+{
+    Q_UNUSED(parent);
+
+    return col_count;
+}
+
+QModelIndex RegsViewModel::mapToSource(const QModelIndex& proxyIndex) const
+{
+    QAbstractItemModel* model = sourceModel();
+    if(model == nullptr) return QModelIndex();
+
+    int row = proxyIndex.row();
+    int col = proxyIndex.column();
+    QModelIndex parent = proxyIndex.parent();
+
+    return model->index(row, col, parent);
+}
+
+QModelIndex RegsViewModel::mapFromSource(const QModelIndex& sourceIndex) const
+{
+    QAbstractItemModel* model = sourceModel();
+    if(model == nullptr) return QModelIndex();
+
+    int row = sourceIndex.row();
+    int col = sourceIndex.column();
+    QModelIndex parent = sourceIndex.parent();
+
+    return model->index(row, col, parent);
+}
 
 Qt::ItemFlags RegsViewModel::flags(const QModelIndex& index) const
 {
@@ -39,7 +115,7 @@ Qt::ItemFlags RegsViewModel::flags(const QModelIndex& index) const
 }
 
 
-bool RegsViewModel::filterAcceptsColumn(int source_column, const QModelIndex& source_parent) const
+/*bool RegsViewModel::filterAcceptsColumn(int source_column, const QModelIndex& source_parent) const
 {
     Q_UNUSED(source_parent);
 
@@ -54,4 +130,5 @@ bool RegsViewModel::filterAcceptsColumn(int source_column, const QModelIndex& so
     }
 
     return false;
-}
+}*/
+
--- regsviewmodel.h
+++ regsviewmodel.h
@@ -1,11 +1,11 @@
 #ifndef REGSVIEWMODEL_H
 #define REGSVIEWMODEL_H
 
-#include <QSortFilterProxyModel>
+#include <QAbstractProxyModel>
 
 class RegListModel;
 
-class RegsViewModel : public QSortFilterProxyModel
+class RegsViewModel : public QAbstractProxyModel
 {
     Q_OBJECT
 public:
@@ -13,10 +13,19 @@ public:
     explicit RegsViewModel(QObject* parent = nullptr);
     ~RegsViewModel();
 
+    // QAbstractItemModel interface
+    QModelIndex index(int row, int column, const QModelIndex& parent) const override;
+    QModelIndex parent(const QModelIndex& child) const override;
+    int rowCount(const QModelIndex& parent) const override;
+    int columnCount(const QModelIndex& parent) const override;
+
+    // QAbstractProxyModel interface
+    QModelIndex mapToSource(const QModelIndex& proxyIndex) const override;
+    QModelIndex mapFromSource(const QModelIndex& sourceIndex) const override;
     Qt::ItemFlags flags(const QModelIndex& index) const override;
 
 protected:
-    bool filterAcceptsColumn(int source_column, const QModelIndex& source_parent) const override;
+    //bool filterAcceptsColumn(int source_column, const QModelIndex& source_parent) const override;
 };
 
 #endif // REGSVIEWMODEL_H
