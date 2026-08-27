c4abad68b0ca825401ea12e0520f1c8cd7950caa
catompiler <artem.lab@gmail.com>
2026-08-27 14:26:29 +0500

    time & voltage formatters unit rule for zero value

--- timeformatter.cpp
+++ timeformatter.cpp
@@ -20,7 +20,7 @@ QString TimeFormatter::format(qreal value, char format, int precision)
 QString TimeFormatter::formatValue(qreal value, char format, int precision)
 {
     static const UnitFormatter::UnitRules unitRules = {
-        {0, QObject::tr("с"), 0},
+        {0, QObject::tr("с"), 0}, // Правило для нулевого значения.
         {1e-6, QObject::tr("мкс"), 1e6},
         {1e-3, QObject::tr("мс"),  1e3},
         {1e0,  QObject::tr("с"),   1e0},
--- voltageformatter.cpp
+++ voltageformatter.cpp
@@ -20,7 +20,7 @@ QString VoltageFormatter::format(qreal value, char format, int precision)
 QString VoltageFormatter::formatValue(qreal value, char format, int precision)
 {
     static const UnitFormatter::UnitRules unitRules = {
-        {0, QObject::tr("В"), 0},
+        {0, QObject::tr("В"), 0}, // Правило для нулевого значения.
         {1e-6, QObject::tr("мкВ"), 1e6},
         {1e-3, QObject::tr("мВ"),  1e3},
         {1e0,  QObject::tr("В"),   1e0},
